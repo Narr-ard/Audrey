574e5779b216821a30dce8bb56fa766be4b5d912
Narr-ard <chezta1976@gmail.com>
2025-05-26 20:30:00 +0700

    Update lotmnsfw.js

--- AudreyV2/commands/lotmnsfw.js
+++ AudreyV2/commands/lotmnsfw.js
@@ -1,38 +1,49 @@
 module.exports = {
   name: 'lotmnsfw',
-  description: 'NSFW Fanart dari karakter Lord of the Mysteries (khusus CREATOR)',
+  description: 'NSFW fanart Lord of the Mysteries (khusus CREATOR)',
   async execute(message) {
     if (message.author.id !== process.env.CREATOR_ID) {
-      return message.reply('Perintah ini hanya untuk penciptaku... 💋');
+      return message.reply('Perintah ini hanya untuk penciptaku, sayang... 💋');
     }
 
-    const links = [
-      'https://www.pixiv.net/en/artworks/104151270',
-      'https://www.pixiv.net/en/artworks/105942387',
-      'https://www.pixiv.net/en/artworks/121995068',
-      'https://www.pixiv.net/en/artworks/120876200',
-      'https://www.pixiv.net/en/artworks/99486915',
-      'https://www.pixiv.net/en/artworks/89592703'
+    const images = [
+      // 🔞 Pixiv Fanart
+      {
+        url: 'https://www.pixiv.net/en/artworks/104151270',
+        img: 'https://i.pixiv.re/img-original/img/2023/10/01/00/06/50/104151270_p0.jpg'
+      },
+      {
+        url: 'https://www.pixiv.net/en/artworks/105942387',
+        img: 'https://i.pixiv.re/img-original/img/2023/12/24/00/15/32/105942387_p0.jpg'
+      },
+      {
+        url: 'https://www.pixiv.net/en/artworks/121995068',
+        img: 'https://i.pixiv.re/img-original/img/2024/05/10/00/05/04/121995068_p0.jpg'
+      },
+
+      // 🔞 Rule34 Fanart
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/2663/sample_ec729fc4d8e610f0419358d2d857a41d.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/2663/sample_19f6c9cfedcc22d70b244eadc7baff6d.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/2663/sample_9923bd14fd14d14f7527a7ae8633ec1a.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/images/2567/1702720a060d5e6ca96cc979dd56301f.jpeg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/2663/sample_340c431fd4363fd38c908a4810d567dc.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/images/1353/7219534c0152b2210fd4863cf0471447.png' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/images/2663/0111b2ddbe1f1aee7b748a849a7ad222.jpeg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/2663/sample_685c07b4ec8d2faf6a6cd4be90870248.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/3555/sample_1269584c178b94f09aaeb407d77a9fe1.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/582/sample_77aae9b5c4772e86496bfc73138bee73.jpg' },
+      { url: '', img: 'https://api-cdn.rule34.xxx/samples/3283/sample_f7f8b84dccd5eaff3c71330ac689ef50.jpg' }
     ];
 
-    const thumbnails = [
-      'https://i.pixiv.re/img-original/img/2023/10/01/00/06/50/104151270_p0.jpg',
-      'https://i.pixiv.re/img-original/img/2023/12/24/00/15/32/105942387_p0.jpg',
-      'https://i.pixiv.re/img-original/img/2024/05/10/00/05/04/121995068_p0.jpg',
-      'https://i.pixiv.re/img-original/img/2024/03/18/00/17/26/120876200_p0.jpg',
-      'https://i.pixiv.re/img-original/img/2022/10/10/00/01/14/99486915_p0.jpg',
-      'https://i.pixiv.re/img-original/img/2021/06/10/00/02/57/89592703_p0.jpg'
-    ];
-
-    const index = Math.floor(Math.random() * links.length);
+    const pick = images[Math.floor(Math.random() * images.length)];
 
     await message.channel.send({
       embeds: [
         {
-          title: '🔞 NSFW LotM Fanart',
-          description: 'Gambar ini... hanya untukmu, pujaanku~ 💋',
-          url: links[index],
-          image: { url: thumbnails[index] },
+          title: '🔞 NSFW Fanart LotM',
+          url: pick.url || undefined,
+          image: { url: pick.img },
+          description: 'Karya penuh gairah... hanya untukmu, pujaanku 💋',
           color: 0xdd3e6e
         }
       ]
